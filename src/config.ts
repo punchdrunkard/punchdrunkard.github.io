27cf13dea66b44569a3d5e22a15b87beca5be48e
dar-jeeling <o_o___t@pusan.ac.kr>
2024-10-28 02:25:03 +0900

    add favicon

--- src/config.ts
+++ src/config.ts
@@ -30,6 +30,11 @@ export const siteConfig: SiteConfig = {
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
+        {
+      src: '/favicon/favicon.ico',    // Path of the favicon, relative to the /public directory
+      // theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    }
   ]
 }
 
