bdd4d9085a208a4857e97916ea79b122fc1a03c0
punchdrunkard <o_o___t@pusan.ac.kr>
2025-05-04 18:42:24 +0900

    chore change site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://punchdrunkard.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
