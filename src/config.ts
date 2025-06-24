7c66997af2efb527f4da35e7e334a79597303951
punchdrunkard <o_o___t@pusan.ac.kr>
2025-06-24 12:48:49 +0900

    change favicon

--- src/config.ts
+++ src/config.ts
@@ -37,7 +37,7 @@ export const siteConfig: SiteConfig = {
 		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		// }
 		{
-			src: "/favicon/favicon.ico", // Path of the favicon, relative to the /public directory
+			src: "/favicon/favicon.PNG", // Path of the favicon, relative to the /public directory
 			// theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 			sizes: "32x32", // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		},
